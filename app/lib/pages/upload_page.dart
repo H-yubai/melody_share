8974a0bdb769125450b3d1858dca102248bee7bf
yu <3225025971@qq.com>
2026-06-23 18:11:47 +0800

    修复安卓webview 错误，提升使用体验

--- app/lib/pages/upload_page.dart
+++ app/lib/pages/upload_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../l10n/app_localizations.dart';
 
@@ -11,32 +12,88 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  InAppWebViewController? _controller;
   bool _isLoading = true;
+  bool _canGoBack = false;
+
+  Future<void> _goBack() async {
+    if (_canGoBack) {
+      await _controller?.goBack();
+    } else {
+      Navigator.pop(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
-    return Scaffold(
-      appBar: AppBar(title: Text(l10n.drawerUploadMusic)),
-      body: Stack(
-        children: [
-          InAppWebView(
-            initialUrlRequest: URLRequest(
-              url: WebUri('https://higequ.com/'),
-            ),
-            initialSettings: InAppWebViewSettings(
-              javaScriptEnabled: true,
-            ),
-            onLoadStart: (controller, url) {
-              if (mounted) setState(() => _isLoading = true);
-            },
-            onLoadStop: (controller, url) {
-              if (mounted) setState(() => _isLoading = false);
-            },
+    return PopScope(
+      canPop: !_canGoBack,
+      onPopInvokedWithResult: (didPop, result) async {
+        if (didPop) return;
+        await _goBack();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(l10n.drawerUploadMusic),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: _goBack,
           ),
-          if (_isLoading)
-            const LinearProgressIndicator(),
-        ],
+        ),
+        body: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: WebUri('https://higequ.com/'),
+              ),
+              initialSettings: InAppWebViewSettings(
+                javaScriptEnabled: true,
+              ),
+              onWebViewCreated: (controller) {
+                _controller = controller;
+              },
+              shouldOverrideUrlLoading:
+                  (controller, navigationAction) async {
+                final uri = navigationAction.request.url;
+                if (uri != null) {
+                  final scheme = uri.scheme;
+                  if (scheme != 'http' && scheme != 'https') {
+                    try {
+                      await launchUrl(
+                        uri,
+                        mode: LaunchMode.externalApplication,
+                      );
+                      return NavigationActionPolicy.CANCEL;
+                    } catch (_) {
+                      try {
+                        await launchUrl(uri);
+                        return NavigationActionPolicy.CANCEL;
+                      } catch (_) {
+                        return NavigationActionPolicy.ALLOW;
+                      }
+                    }
+                  }
+                }
+                return NavigationActionPolicy.ALLOW;
+              },
+              onLoadStart: (controller, url) {
+                if (mounted) setState(() => _isLoading = true);
+              },
+              onLoadStop: (controller, url) async {
+                if (mounted) {
+                  final canGoBack = await controller.canGoBack();
+                  setState(() {
+                    _isLoading = false;
+                    _canGoBack = canGoBack;
+                  });
+                }
+              },
+            ),
+            if (_isLoading)
+              const LinearProgressIndicator(),
+          ],
+        ),
       ),
     );
   }
--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import media_kit_libs_macos_audio
 import path_provider_foundation
 import shared_preferences_foundation
 import sqlite3_flutter_libs
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
@@ -21,4 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
